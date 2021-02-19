4021b7db417e872384515c619e6ebea543bd40a8
pshtony1 <pshtony1@naver.com>
2021-02-19 10:28:41 +0900

    JSX & Props

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# React Practice
+# Movie App | React Practice
 
-`React`가, `SPA Framework`가 도대체 뭐길래 찍먹해보는 프로젝트.
+`React`가, `SPA Framework`가 도대체 뭐길래 찍먹해보는 첫 React 프로젝트
+
+---
+
+### React에서 `Emnet` 사용하기
+
+`JSX` 작성할때 `Enmet` 안되서 불편했음.
+
+1. VSCode 설정 열기
+
+   - Windows: `Ctrl + ,`
+   - Mac OS: `Command + ,`
+
+2. WorkSpace 설정에 들어가기
+
+3. settings.json 열기
+
+4. 아래 코드 넣어주기
+
+```
+{
+   "emmet.includeLanguages": {
+      "javascript": "javascriptreact"
+   }
+}
+```
--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+
+const foods = [
+  {
+    id: 1,
+    name: "kimchi",
+    stock: {
+      left: false,
+      stocks: 0,
+    },
+  },
+  {
+    id: 2,
+    name: "bibim-bap",
+    stock: {
+      left: true,
+      stocks: 3,
+    },
+  },
+];
+
+function Food({ name, stock }) {
+  return (
+    <div className="food">
+      <h3 className="food__name">{name}</h3>
+      {stock.left ? (
+        <h5 className="food__stock">{stock.stocks} Left</h5>
+      ) : (
+        <h5 className="food__stock">Sold Out</h5>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is Foods!</h1>
+      {foods.map((food) => (
+        <Food key={food.id} name={food.name} stock={food.stock} />
+      ))}
     </div>
   );
 }
